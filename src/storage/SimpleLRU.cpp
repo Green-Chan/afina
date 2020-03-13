5eb3820b26c772a5769bf0f126c0bea1806efb32
Green-Chan <55838532+Green-Chan@users.noreply.github.com>
2020-03-13 16:47:20 +0300

    Update SimpleLRU.cpp

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -61,6 +61,25 @@ bool SimpleLRU::Get(const std::string &key, std::string &value) {
     return true;
 }
 
+void SimpleLRU::to_head(lru_node *node_ptr) {
+    if (node_ptr != _lru_head.get()) {
+        if (!node_ptr->next) {
+            node_ptr->next = std::move(_lru_head);
+            _lru_head = std::move(node_ptr->prev->next);
+            node_ptr->prev->next = nullptr;
+        } else {
+            node_ptr->next->prev = node_ptr->prev;
+            std::unique_ptr<lru_node> new_head = std::move(node_ptr->prev->next);
+            node_ptr->prev->next = std::move(node_ptr->next);
+            node_ptr->next = std::move(_lru_head);
+            _lru_head = std::move(new_head);
+            new_head = nullptr;
+            node_ptr->prev = node_ptr->next->prev;
+            node_ptr->next->prev = node_ptr;
+        }
+    } 
+}
+
 void SimpleLRU::set(lru_node *node_ptr, const std::string &value) {
     to_head(node_ptr);
     _curr_size -= node_ptr->value.size();
@@ -69,7 +88,9 @@ void SimpleLRU::set(lru_node *node_ptr, const std::string &value) {
         while (value.size() > _max_size - _curr_size) {
             last_deleted = last_deleted->prev;
             last_deleted->next.reset();
-            _lru_index.erase(_lru_index.find(std::cref(last_deleted->key)));
+            auto it = _lru_index.find(std::cref(last_deleted->key));
+            assert(it != _lru_index.end());
+            _lru_index.erase(it);
             _curr_size -= last_deleted->key.size() + last_deleted->value.size();
         }
         _lru_head->prev = last_deleted->prev;
@@ -96,14 +117,12 @@ void SimpleLRU::put(const std::string &key, const std::string &value)  {
     }
     _curr_size += key.size() + value.size();
     if (!_lru_head) {
-        _lru_head = std::make_unique<lru_node>();
-        _lru_head->key = key;
+        _lru_head = std::unique_ptr<lru_node>(new lru_node(key));
         _lru_head->value = value;
         _lru_head->prev = _lru_head.get();
         _lru_head->next = nullptr;
     } else {
-        std::unique_ptr<lru_node> new_node = std::make_unique<lru_node>();
-        new_node->key = key;
+        std::unique_ptr<lru_node> new_node = std::unique_ptr<lru_node>(new lru_node(key));
         new_node->value = value;
         new_node->prev = _lru_head->prev;
         _lru_head->prev = new_node.get();
